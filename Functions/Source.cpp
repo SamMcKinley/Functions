dccd29b3cc57129781270ef9dba82fea937dc8ab
SAMUEL MCKINLEY <SAMMCKIN@uat.edu>
2020-03-03 14:21:07 -0700

    Added more to the story

--- Functions/Source.cpp
+++ Functions/Source.cpp
@@ -4,32 +4,41 @@
 using namespace std;
 
 //prototype functions
-int AddsTwoNumber(int firstNumber, int secondNumber);
+string GetUserName();
+void NewEmployeeWelcome(string userName);
+string Scenario 1;
+string Scenario 2;
 
 
 int main()
 {
 	//Show title of game sim
 	cout << "\n\n\t\t *** Welcome to the Advanced Technology Lab Sim!***\n\n";
-	//we need a variable to catch the sum which is the
-	//return value
-	int catchTheSum = 0;
-	catchTheSum = AddsTwoNumber(5, 7);
-	//Display the answer the function gave us to the screen
-	cout << "\nMy calculations say the sum is " << catchTheSum << endl;
-
-
+	// Creating a string variable to hold the user name:
+	string userName;
+	// Call function to get user name
+	userName = GetUserName();
+	// Display into to user
+	NewEmployeeWelcome(userName);
+	//This return will end the game
 	return 0;
 }
 
-int AddsTwoNumber(int firstNumber, int secondNumber)
+string GetUserName()
 {
-	//Print what the function is doing
-	cout << "\nI am calculating the two numbers!\n";
-	//Create variable to hold the sum
-	int sum;
-	//add two numbers and place it into the sum
-	sum = firstNumber + secondNumber;
-	//returns the sum from the function
-	return sum;
+	// Creating a string variable to hold the user name:
+	string userName;
+	//telling the user to enter their name.
+	cout << "\nWhat is your name?"
+	//getting the name from user
+	getline(cin, userName);
+	//return name
+	return userName;
+}
+
+void NewEmployeeWelcome(string userName)
+{
+	cout << "\nWelcome to Advanced Technology Lab LB-00\n";
+	cout << "\nYou arrive at the classified location of LB-00, ready for your first day of work.\n ";
+	cout << "\nYou hear a voice over the intercom that says 'Welcome, new empoyee! Please make your way to room 01 and pick up the items that you will require for your first day.'\n ";
 }
